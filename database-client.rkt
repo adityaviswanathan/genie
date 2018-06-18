dab6cb33c389148d9821fdbcdc1d1d6202ecb90f
Aditya Viswanathan <aditya@adityaviswanathan.com>
2018-06-17 23:29:20 -0500

    Improvements to query API via expressive filter support.

--- database-client.rkt
+++ database-client.rkt
@@ -53,7 +53,7 @@
        (query-exec cxn
          (format "grant all privileges on database ~a to ~a" dbname dbuser))))
 
-;; Executes SQL command to generates a new database from @db proto and @old-cxn
+;; Executes SQL command to generate a new database from @db proto and @old-cxn
 ;; and generates a new connection to the newly created database.
 ;; db: Database proto to be generated.
 ;; old-cxn: Old database connection. Replaced with returned new connection.
@@ -92,66 +92,112 @@
 ;; cxn: Active connection to database.
 ;; query: Query proto object used to execute the query against database.
 (define (query->rows cxn query)
-  (define (projections->queryfrag projs)
+  ;; Maps @unwrapfunc to each element in @lst and comma delimits the
+  ;; resulting list into a string of the form "elemA, elemB, elemC".
+  ;; lst: List to comma delimit.
+  ;; unwrapfunc: Func to apply to each element in @lst before inserting into
+  ;;             into comma delimited string list.
+  (define (comma-delimit lst unwrapfunc)
     (string-append* (cdr (append* (map
-      (lambda (i) (list ", " i)) (map proto-query-projection-column projs))))))
-  (define (sources->queryfrag sources)
-    (string-append* (cdr (append* (map
-      (lambda (i) (list ", " i)) (map proto-query-source-table sources))))))
+      (lambda (i) (list ", " i)) (map unwrapfunc lst))))))
+  ;; Slices @lst until but not including index @end.
   (define (list->prefix lst end)
     (append
       (for/list ([i (in-range end)])
                 (list-ref lst i))))
-  (define (filterlist->length fltrlist idx)
+  ;; Computes the recursive length of a FilterList proto.
+  ;; fltrlist: FilterList proto whose length will be computed.
+  ;; idx: Index of @fltrlist, used to control recursive length calculation.
+  (define (filterlist->index fltrlist idx)
+    ;; Slice of @fltrlist up till @idx.
     (define prefix (list->prefix (proto-query-filter-list-lists fltrlist) idx))
-    (define childlengths
+    (define curr 
       (for/list ([i (in-range (length prefix))])
-                (filterlist->length (list-ref prefix i) (length (proto-query-filter-list-lists (list-ref prefix i))))))
+        (filterlist->index (list-ref prefix i)
+                           (length (proto-query-filter-list-lists (list-ref prefix i))))))
     (apply +
-      (append (map length (map proto-query-filter-list-filters prefix))
-              childlengths)))
-  (define (filterlist-contents->queryfrag fltrlist curridx)
+      (append (map length (map proto-query-filter-list-filters prefix)) curr)))
+  ;; Generates a list of the contents of a FilterList proto. The filters are in
+  ;; the output list are placeholder strings of the form "<COL> = $<IDX>".
+  ;; fltrlist: FilterList proto for which the contents will be generated.
+  ;; curridx: Start index of this list for recursive traversal of FilterList.
+  (define (filterlist->filters fltrlist curridx)
+    (define openparen "(")
+    (define closeparen ")")
+    (define equals " = $") ;; TODO(aditya): Generalize filter operator support.
     (append
       (for/list ([i (in-range (length (proto-query-filter-list-filters fltrlist)))])
-                (string-append (proto-query-filter-column (list-ref (proto-query-filter-list-filters fltrlist) i))
-                                " = $" ;; TODO(aditya): Generalize filter operator support.
-                                (number->string (+ curridx (+ i 1)))))
+        (define fltr (list-ref (proto-query-filter-list-filters fltrlist) i))
+        (string-append (proto-query-filter-column fltr)
+                        equals
+                        (number->string (+ curridx (+ i 1)))))
       (for/list ([i (in-range (length (proto-query-filter-list-lists fltrlist)))])
-                (string-append "("
-                               (filterlist->queryfrag
-                                 (list-ref (proto-query-filter-list-lists fltrlist) i)
-                                 (+ curridx (+ (filterlist->length fltrlist i)
-                                               (length (proto-query-filter-list-filters fltrlist)))))
-                               ")"))))
+        (define fltr (list-ref (proto-query-filter-list-lists fltrlist) i))
+        (define baseindex (length (proto-query-filter-list-filters fltrlist)))
+        (define deepindex (filterlist->index fltrlist i))
+        (string-append openparen 
+                       (filterlist->queryfrag fltr (+ curridx (+ baseindex deepindex)))
+                       closeparen))))
+  ;; Emits the SQL equivalent of an ordered traversal of the composite filters
+  ;; in a FilterList proto and returns a SQL fragment of the form:
+  ;; "<COL> = $<IDX> <CONNECTIVE> ..." e.g. "name = $1 and type = $2".
+  ;; This is used in the query-rows call later. Note that filter values are not
+  ;; inserted into SQL here but are instead bound via the query-rows call and
+  ;; the production of the filter value list via filterlist->values.
+  ;; fltrlist: FilterList proto to be traversed.
+  ;; curridx: Index of the current FilterList, required for recursive traversal.
   (define (filterlist->queryfrag fltrlist curridx)
+    ;; The sum of this FilterList's filters and nested FilterLists is the
+    ;; net contribution of this FilterList to the overall count of filters in
+    ;; the query and tells us how many connectives are required for this frag.
+    (define currlength (+ (length (proto-query-filter-list-filters fltrlist))
+                          (length (proto-query-filter-list-lists fltrlist))))
+    ;; Build the SQL frag by constructing the current FilterProto artifacts
+    ;; and recursing on all nested FilterProtos.
     (string-append*
       (cdr (append*
         (map (lambda (connective col) (list connective col))
-          (for/list ([i (in-range (+ (length (proto-query-filter-list-filters fltrlist))
-                                     (length (proto-query-filter-list-lists fltrlist))))])
-            (case (proto-query-filter-list:e->integer (proto-query-filter-list-connective fltrlist))
+          (for/list ([i (in-range currlength)])
+            (case (proto-query-filter-list:e->integer
+                    (proto-query-filter-list-connective fltrlist))
               [(0) " and "]
               [(1) " or "]))
-          (filterlist-contents->queryfrag fltrlist curridx))))))
+          (filterlist->filters fltrlist curridx))))))
+  ;; SQL statement skeleton for DB query. Currently supports placeholders for
+  ;; projections, sources, and filters in a "select ... from ... where ... "
+  ;; statement.
   (define sql-query
     (format "select ~a from ~a where ~a"
-            (projections->queryfrag (proto-query-projections query))
-            (sources->queryfrag (proto-query-sources query))
+            (comma-delimit (proto-query-projections query)
+                           proto-query-projection-column)
+            (comma-delimit (proto-query-sources query)
+                           proto-query-source-table)
             (filterlist->queryfrag (proto-query-filterlist query) 0)))
-  (define (filterlist->filters fltrlist)
+  ;; Flattens a list of FilterList protos into an ordered list of their
+  ;; corresponding values.
+  ;; fltrlist: FilterList proto from which to produce a list of values.
+  (define (filterlist->values fltrlist)
+    ;; Because FilterList proto can nest other FilterLists, we first extract
+    ;; the top level filter values and then append to the values extracted from
+    ;; each nested FilterList.
+    ;; Collect top-level filter values.
     (define basefilters
       (for/list ([i (in-range (length (proto-query-filter-list-filters fltrlist)))])
-        (list (case (proto-datatype:e->integer (proto-query-filter-datatype (list-ref (proto-query-filter-list-filters fltrlist) i)))
-                [(0) (proto-query-filter-intvalue (list-ref (proto-query-filter-list-filters fltrlist) i))]
-                [(1) (proto-query-filter-floatvalue (list-ref (proto-query-filter-list-filters fltrlist) i))]
-                [(2) (proto-query-filter-stringvalue (list-ref (proto-query-filter-list-filters fltrlist) i))]
-                [(3) (proto-query-filter-boolvalue (list-ref (proto-query-filter-list-filters fltrlist) i))]))))
+        (define fltr (list-ref (proto-query-filter-list-filters fltrlist) i))
+        (list (case (proto-datatype:e->integer
+                      (proto-query-filter-datatype fltr))
+                [(0) (proto-query-filter-intvalue fltr)]
+                [(1) (proto-query-filter-floatvalue fltr)]
+                [(2) (proto-query-filter-stringvalue fltr)]
+                [(3) (proto-query-filter-boolvalue fltr)]))))
+    ;; Recurse on each nested FilterList.
     (define deepfilters
-      (map filterlist->filters (proto-query-filter-list-lists fltrlist)))
+      (map filterlist->values (proto-query-filter-list-lists fltrlist)))
+    ;; Collect all filter values on all levels and flatten into single list.
     (flatten (append basefilters deepfilters)))
   (apply query-rows
     (append (list cxn sql-query)
-            (filterlist->filters (proto-query-filterlist query)))))
+            (filterlist->values (proto-query-filterlist query)))))
 
 ;; Query API to Database. Using this syntax, we can query the database using a
 ;; Query proto and conntection e.g. "(db-query cxn query)".
